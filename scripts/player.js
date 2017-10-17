9ddbe06e23d26e7163054de3e42c4c9bb66ba7bf
DEVIN TASSIE <devintassie@DEVINs-MacBook-Pro.local>
2017-10-17 16:52:45 -0500

    fixed repeating numbers on song list

--- scripts/player.js
+++ scripts/player.js
@@ -6,6 +6,11 @@ class Player {
     this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
   }
 
+  prettyTime (timeInSeconds) {
+
+    const totalTime = (timeInSeconds / 60);
+    return totalTime;
+}
   getDuration() {
     return this.soundObject.getDuration();
   }
@@ -13,14 +18,14 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +42,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +55,3 @@ class Player {
 }
 
 const player = new Player();
-
--- scripts/song-list.js
+++ scripts/song-list.js
@@ -3,7 +3,6 @@ album.songs.forEach( (song, index) => {
   song.element = $(`
 
     <tr>
-         <td>${index + 1}</td>
          <td>
            <button>
              <span class="song-number">${index + 1}</span>
